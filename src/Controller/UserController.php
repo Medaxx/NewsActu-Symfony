71a38cbef002fca8bb1887ab505f9e58d7371e43
Mehdi <meme.hamidi75@gmail.com>
2022-03-21 16:59:59 +0100

    created User

---
+++ src/Controller/UserController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\RegisterFormTypePhpType;
+use DateTime;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+class UserController extends AbstractController
+{
+    /**
+     * @Route("/inscription", name="user_register", methods={"GET|POST"})
+     */
+    public function register(Request $request, EntityManagerInterface $entityManager, UserPasswordHasherInterface $passwordHasher): Response
+    {
+        $user = new User();
+        $form = $this->createForm(RegisterFormTypePhpType::class, $user)
+        ->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+
+            $user->setRoles(['ROLE_USER']);
+            $user->setCreatedAt(new DateTime());
+            $user->setUpdatedAt(new DateTime());
+
+            # Hashage du password en clair
+            $user->setPassword($passwordHasher->hashPassword($user, $user->getPassword()));
+
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            $this->addFlash('sucess', 'Vous êtes maintenant inscrit. Bienvenue !');
+
+            return $this->redirectToRoute('default_home');
+        }
+        
+        return $this->render('user/register.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+}
